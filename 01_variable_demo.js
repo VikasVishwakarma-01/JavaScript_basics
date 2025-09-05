c31ccdf21eb682347aef7a2d5daeccf432e6e508
vikas <vikasweb77@gmail.com>
2025-09-06 00:20:52 +0530

    basic code

---
+++ 01_variable_demo.js
@@ -0,0 +1,43 @@
+var player_name1 = "Vikas";
+var score1 = 10;
+
+let player_name2 = "Mohan";
+let score2 = 20;
+
+const player_name3 = "Mahi";
+const score3 = 30;
+
+console.log(player_name1, score1);
+console.log(player_name2, score2);
+console.log(player_name3, score3);
+
+// one complete instruction is semicolon
+
+// Rules for varaible name :- 
+// small, capital, digits, _ , $
+// can't have space or any other special char 
+// varaible name should be meaningful
+// variable name can't start with a digit and cant have in b/w
+// can't use keywords
+
+// in built function ---> console.log()
+// each console.log() will display in different line ans can take multiple variables
+
+// datatype -> number, string, boolean, 
+// string -> (" ", ` `, \ \)
+// boolean -> true, false, undefined (defined later)
+
+// objects :- (key, value) pair 
+{
+    name : "vikas";
+    age: 21;
+    {
+        {
+            
+        }
+    }
+}
+
+// primitive and non primitive
+
+
---
+++ 02_datatype.js
@@ -0,0 +1,20 @@
+var marks = 100;
+var name = "Vikas";
+var age = 21;
+var company = null;
+var salary = undefined;
+var isStudent = true;
+// console.log("Marks = ", marks);
+// console.log("Name = ", name);
+// console.log("Age = ", age);
+// console.log("Company = ", company);
+// console.log("Salary = ", salary);
+// console.log("IsStudent = ", isStudent);
+
+var user = {
+    marks: 100,
+    name: "Vikas",
+    age: 21
+}
+
+console.log(user);
